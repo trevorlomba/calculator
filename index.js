2bce0789e791824cf9327dadecdccf6009f8deb3
jcornmanhomonoff <jessica.cornman-homonoff@generalassemb.ly>
2018-07-13 10:20:43 -0400

    Add babel-polyfill to dependencies
    - Add support for `async` and `await`, and other new JS features like
      Map, Set, etc. by adding `babel-polyfill`
    - Close #143

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 // user require with a reference to bundle the file and use it in this file
 // var example = require('./example');
 
+// allows usage of new JS features
+require('babel-polyfill')
+
 // load manifests
 // scripts
 require('./assets/scripts/app.js')
